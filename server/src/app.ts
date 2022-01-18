e55d3ba3ce4448e151cca3ceaa823392bede7ba6
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2022-01-17 22:40:47 -0300

    🔧 tweaked typescript config

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  res.send(`My name is `);
 });
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
