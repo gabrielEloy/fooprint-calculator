46d06a6d46ffdbefa50bfc5c1ea75334f91d8e40
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2022-01-17 23:18:28 -0300

    🔧 added eslint

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,8 +3,6 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send(`My name is `);
+  res.send(`My name is ${2 + 2}`);
 });
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
-});
+app.listen(port, () => console.log(`server is listening on ${port}`));
