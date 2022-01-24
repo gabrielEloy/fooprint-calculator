741874f414c475f6e0f4634e90b9e8f5583f4f17
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2022-01-23 21:35:15 -0300

    ✨ fixed BE cors config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import emissionCategoryRouter from '@src/routes/emissionCategories';
+import cors from 'cors';
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = 4000 || process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/emissions', emissionCategoryRouter);
 
